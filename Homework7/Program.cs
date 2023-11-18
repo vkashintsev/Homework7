d6acee6dc10814fe1c8e32f6c450520f64ac71e3
Vladimir Kashintsev <kashintsevvs@gmail.com>
2023-11-18 18:57:12 +0300

    Add project files.

---
+++ Homework7/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    { 
+        Sample(100000);
+        Sample(1000000);
+        Sample(10000000);
+        Console.WriteLine($"* Время измеряется в тиках процессора");
+        Console.ReadKey();
+    }
+    private static void Sample(int length)
+    {
+        var arr = new int[length]; 
+        for (int i = 0; i < length; i++)
+        {
+            arr[i] = i;
+        }
+        var list = new List<int>(arr);
+        var stopwatch = new Stopwatch();
+        Console.WriteLine($"Число элементов: {length}");
+        stopwatch.Start();
+        var sum1 = Sum1(arr);
+        stopwatch.Stop();
+        Console.WriteLine($"Обычное Время: {stopwatch.ElapsedTicks}");
+        stopwatch.Start();
+        var sum2 = Sum2(list);
+        Console.WriteLine($"Thread  Время: {stopwatch.ElapsedTicks}");
+        stopwatch.Stop();
+        stopwatch.Start();
+        var sum3 = Sum3(arr);
+        stopwatch.Stop();
+        Console.WriteLine($"LINQ    Время: {stopwatch.ElapsedTicks}"); 
+    }
+
+
+
+    private static long Sum1(int[] input)
+    {
+        long sum = 0;
+        foreach (var item in input)
+        {
+            sum += item;
+        }
+        return sum;
+
+    } 
+    private static long Sum2(List<int> input)
+    {
+        long sum = 0;
+        Parallel.ForEach(input, value =>
+        {
+            sum += value;
+        });
+        return sum;
+    }
+    private static long Sum3(int[] input)
+    {
+        long sum = 0;
+        Array.ForEach(input, delegate (int i) { sum += i; });
+        return sum;
+    }
+
+
+}
